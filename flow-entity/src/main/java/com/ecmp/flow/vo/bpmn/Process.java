555fd5678415b24ceeab1b903f19e0bf3e246b73
yizicangyin@qq.com <yizicangyin@qq.com>
2017-04-12 09:20:34 +0800

    流程转换相关类

---
+++ flow-entity/src/main/java/com/ecmp/flow/vo/bpmn/Process.java
@@ -0,0 +1,164 @@
+package com.ecmp.flow.vo.bpmn;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import javax.xml.bind.annotation.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * *************************************************************************************************
+ * <p/>
+ * 实现功能：流程类
+ * <p>
+ * ------------------------------------------------------------------------------------------------
+ * 版本          变更时间             变更人                     变更原因
+ * ------------------------------------------------------------------------------------------------
+ * 1.0.00      2017/4/10 9:52      陈飞(fly)                  新建
+ * <p/>
+ * *************************************************************************************************
+ */
+@XmlType(name = "process")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Process extends BaseNode implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 流程名
+     */
+    @XmlAttribute
+    private String name;
+    /**
+     * 是否可执行
+     */
+    @XmlAttribute
+    private boolean isExecutable;
+
+    /**
+     * 启动条件
+     */
+    @XmlTransient
+    private String startUEL;
+
+    @XmlTransient
+    private JSONObject nodes;
+
+    private List<StartEvent> startEvent;
+    private List<EndEvent> endEvent;
+    private List<UserTask> userTask;
+    private List<MailTask> mailTask;
+    private List<ManualTask> manualTask;
+    private List<ScriptTask> scriptTask;
+    private List<ServiceTask> serviceTask;
+    private List<ExclusiveGateway> exclusiveGateway;
+    private List<InclusiveGateway> inclusiveGateway;
+    private List<ParallelGateway> parallelGateway;
+    private List<EventGateway> eventGateway;
+    private List<SequenceFlow> sequenceFlow;
+
+    @XmlTransient
+    private int lineCount = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isExecutable() {
+        return isExecutable;
+    }
+
+    public void setExecutable(boolean executable) {
+        isExecutable = executable;
+    }
+
+    public String getStartUEL() {
+        return startUEL;
+    }
+
+    public void setStartUEL(String startUEL) {
+        this.startUEL = startUEL;
+    }
+
+    public JSONObject getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(JSONObject nodes) {
+        this.nodes = nodes;
+        if (nodes != null) {
+            startEvent = new ArrayList<StartEvent>();
+            endEvent = new ArrayList<EndEvent>();
+            userTask = new ArrayList<UserTask>();
+            mailTask = new ArrayList<MailTask>();
+            manualTask = new ArrayList<ManualTask>();
+            scriptTask = new ArrayList<ScriptTask>();
+            serviceTask = new ArrayList<ServiceTask>();
+            exclusiveGateway = new ArrayList<ExclusiveGateway>();
+            inclusiveGateway = new ArrayList<InclusiveGateway>();
+            parallelGateway = new ArrayList<ParallelGateway>();
+            eventGateway = new ArrayList<EventGateway>();
+            sequenceFlow = new ArrayList<SequenceFlow>();
+            lineCount = 0;
+            Iterator iterator = nodes.keys();
+            while (iterator.hasNext()) {
+                String key = (String) iterator.next();
+                JSONObject node = nodes.getJSONObject(key);
+                JSONArray targets = node.getJSONArray("target");
+                switch (node.getString("type")) {
+                    case "StartEvent":
+                        startEvent.add((StartEvent) JSONObject.toBean(node, StartEvent.class));
+                        break;
+                    case "EndEvent":
+                        endEvent.add((EndEvent) JSONObject.toBean(node, EndEvent.class));
+                        break;
+                    case "UserTask":
+                        userTask.add((UserTask) JSONObject.toBean(node, UserTask.class));
+                        break;
+                    case "MailTask":
+                        mailTask.add((MailTask) JSONObject.toBean(node, MailTask.class));
+                        break;
+                    case "ManualTask":
+                        manualTask.add((ManualTask) JSONObject.toBean(node, ManualTask.class));
+                        break;
+                    case "ScriptTask":
+                        scriptTask.add((ScriptTask) JSONObject.toBean(node, ScriptTask.class));
+                        break;
+                    case "ServiceTask":
+                        serviceTask.add((ServiceTask) JSONObject.toBean(node, ServiceTask.class));
+                        break;
+                    case "ExclusiveGateway":
+                        exclusiveGateway.add((ExclusiveGateway) JSONObject.toBean(node, ExclusiveGateway.class));
+                        break;
+                    case "InclusiveGateway":
+                        inclusiveGateway.add((InclusiveGateway) JSONObject.toBean(node, InclusiveGateway.class));
+                        break;
+                    case "ParallelGateway":
+                        parallelGateway.add((ParallelGateway) JSONObject.toBean(node, ParallelGateway.class));
+                        break;
+                    case "EventGateway":
+                        eventGateway.add((EventGateway) JSONObject.toBean(node, EventGateway.class));
+                        break;
+                    default:
+                        break;
+                }
+                addSequenceFlow(key, targets);
+            }
+        }
+    }
+
+    private void addSequenceFlow(String sourceId, JSONArray targets) {
+        for (int i = 0; i < targets.size(); i++) {
+            String id = "flow" + (++lineCount);
+            String targetId = targets.getString(i);
+            SequenceFlow sf = new SequenceFlow(id, sourceId, targetId);
+            sequenceFlow.add(sf);
+        }
+    }
+}
---
+++ flow-service/src/main/java/com/ecmp/flow/util/XmlUtil.java
@@ -0,0 +1,36 @@
+package com.ecmp.flow.util;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
+/**
+ * *************************************************************************************************
+ * <p/>
+ * 实现功能：xml
+ * <p>
+ * ------------------------------------------------------------------------------------------------
+ * 版本          变更时间             变更人                     变更原因
+ * ------------------------------------------------------------------------------------------------
+ * 1.0.00      2017/4/12 9:04      陈飞(fly)                  新建
+ * <p/>
+ * *************************************************************************************************
+ */
+public class XmlUtil {
+
+    /**
+     * javabean转xml
+     * @param entity
+     * @return
+     */
+    public String serialize(Object entity) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(entity.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(entity, writer);
+        return writer.toString();
+    }
+}
