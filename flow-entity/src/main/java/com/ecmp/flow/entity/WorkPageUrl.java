be767af745e3716476e0260137bd65f413c80ceb
tanjun <827200436@qq.com>
2017-04-13 13:31:10 +0800

    工作界面配置管理接口代码

---
+++ flow-entity/src/main/java/com/ecmp/flow/entity/WorkPageUrl.java
@@ -0,0 +1,111 @@
+package com.ecmp.flow.entity;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+
+
+/**
+ * *************************************************************************************************
+ * <p/>
+ * 实现功能：
+ * 工作界面配置管理Entity定义
+ * <p>
+ * ------------------------------------------------------------------------------------------------
+ * 版本          变更时间             变更人                     变更原因
+ * ------------------------------------------------------------------------------------------------
+ * 1.0.00      2017/3/21 10:20      谭军(tanjun)                新建
+ * <p/>
+ * *************************************************************************************************
+ */
+
+@Entity(name = "flow_workPageUrl")
+@DynamicInsert
+@DynamicUpdate
+public class WorkPageUrl extends com.ecmp.core.entity.BaseEntity {
+    /**
+     * 名称
+     */
+    @Column(length = 80, nullable = false)
+    private String name;
+
+    /**
+     * URL界面地址
+     */
+    @Lob
+    private String url;
+
+    /**
+     * 描述
+     */
+    @Column(length = 250)
+    private String depict;
+
+    /**
+     * 关联的应用模块ID
+     */
+    @Column(length = 36,name = "appModule_id")
+    private String appModuleId;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDepict() {
+        return depict;
+    }
+
+    public void setDepict(String depict) {
+        this.depict = depict;
+    }
+
+    public String getAppModuleId() {
+        return appModuleId;
+    }
+
+    public void setAppModuleId(String appModuleId) {
+        this.appModuleId = appModuleId;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", this.getId())
+                .append("name", this.name)
+                .append("appModuleId", this.appModuleId)
+                .append("url", this.url)
+                .append("depict",this.depict)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+}
---
+++ flow-service/src/main/java/com/ecmp/flow/service/WorkPageUrlService.java
@@ -0,0 +1,27 @@
+package com.ecmp.flow.service;
+
+import com.ecmp.core.service.BaseService;
+import com.ecmp.flow.api.IWorkPageUrlService;
+import com.ecmp.flow.dao.WorkPageUrlDao;
+import com.ecmp.flow.entity.WorkPageUrl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * *************************************************************************************************
+ * <p/>
+ * 实现功能：工作界面配置管理
+ * <p>
+ * ------------------------------------------------------------------------------------------------
+ * 版本          变更时间             变更人                     变更原因
+ * ------------------------------------------------------------------------------------------------
+ * 1.0.00      2017/3/23 22:39      谭军(tanjun)               新建
+ * <p/>
+ * *************************************************************************************************
+ */
+@Service
+public class WorkPageUrlService extends BaseService<WorkPageUrl, String> implements IWorkPageUrlService {
+
+    @Autowired
+    private WorkPageUrlDao workPageUrl;
+}
