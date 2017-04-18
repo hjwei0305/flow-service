02f22676a6ef63988a2e5d2e9e5e74a0024deb5f
yizicangyin@qq.com <yizicangyin@qq.com>
2017-04-18 10:53:14 +0800

    流程节点配置

--- flow-entity/src/main/java/com/ecmp/flow/vo/bpmn/Definition.java
+++ flow-entity/src/main/java/com/ecmp/flow/vo/bpmn/Definition.java
@@ -1,13 +1,8 @@
 package com.ecmp.flow.vo.bpmn;
 
-import net.sf.json.JSONObject;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+import javax.persistence.Transient;
 import javax.xml.bind.annotation.*;
 import java.io.Serializable;
-import java.io.StringWriter;
 
 /**
  * *************************************************************************************************
@@ -41,6 +36,12 @@ public class Definition implements Serializable {
     @XmlElement
     private Process process;
 
+    /**
+     * 前端设计json文本
+     */
+    @Transient
+    private String defJson;
+
     public Process getProcess() {
         return process;
     }
@@ -49,21 +50,12 @@ public class Definition implements Serializable {
         this.process = process;
     }
 
-    public static void main(String[] args) throws JAXBException {
-        Definition df = new Definition();
-        Process process = new Process();
-        process.setName("测试");
-        process.setId("1122");
-        process.setStartUEL("23sfsfsf");
-        JSONObject nodes = JSONObject.fromObject("{\"StartEvent_0\":{\"type\":\"StartEvent\",\"x\":67,\"y\":156,\"id\":\"StartEvent_0\",\"target\":[\"UserTask_1\"],\"name\":\"开始\"},\"UserTask_1\":{\"type\":\"UserTask\",\"x\":246,\"y\":259,\"id\":\"UserTask_1\",\"target\":[\"ExclusiveGateway_2\"],\"name\":\"审批任务\"},\"ExclusiveGateway_2\":{\"type\":\"ExclusiveGateway\",\"x\":529,\"y\":238,\"id\":\"ExclusiveGateway_2\",\"target\":[\"UserTask_3\",\"UserTask_4\"],\"name\":\"排他网关\"},\"UserTask_3\":{\"type\":\"UserTask\",\"x\":716,\"y\":120,\"id\":\"UserTask_3\",\"target\":[\"EndEvent_5\"],\"name\":\"审批任务\"},\"UserTask_4\":{\"type\":\"UserTask\",\"x\":674,\"y\":386,\"id\":\"UserTask_4\",\"target\":[\"EndEvent_5\"],\"name\":\"审批任务\"},\"EndEvent_5\":{\"type\":\"EndEvent\",\"x\":965,\"y\":223,\"id\":\"EndEvent_5\",\"target\":[],\"name\":\"结束\"}}");
-        process.setNodes(nodes);
-        df.setProcess(process);
-        JAXBContext context = JAXBContext.newInstance(df.getClass());
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
-        StringWriter writer = new StringWriter();
-        marshaller.marshal(df, writer);
-        System.out.println(writer.toString());
+    public String getDefJson() {
+        return defJson;
     }
+
+    public void setDefJson(String defJson) {
+        this.defJson = defJson;
+    }
+
 }
