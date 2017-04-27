a2cf95c46b53020240e61d465542494a7fc6a49c
tanjun <827200436@qq.com>
2017-04-27 09:55:17 +0800

    流程撤销重定义

---
+++ flow-dao/src/main/java/com/ecmp/flow/dao/FlowHiVarinstDao.java
@@ -0,0 +1,11 @@
+package com.ecmp.flow.dao;
+
+import com.ecmp.core.dao.jpa.BaseDao;
+import com.ecmp.flow.entity.FlowHiVarinst;
+import com.ecmp.flow.entity.WorkPageUrl;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FlowHiVarinstDao extends BaseDao<FlowHiVarinst, String> {
+
+}
