9907adde599a59c598737767c48451bcea20ced3
zhanyao <708845433@qq.com>
2017-05-11 20:28:22 +0800

    BusinessWorkPageUrlService

--- flow-service/src/main/java/com/ecmp/flow/service/BusinessWorkPageUrlService.java
+++ flow-service/src/main/java/com/ecmp/flow/service/BusinessWorkPageUrlService.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.awt.*;
