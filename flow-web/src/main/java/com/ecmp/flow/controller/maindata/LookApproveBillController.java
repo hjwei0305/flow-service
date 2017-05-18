8ebce4058d4d9cff4b3d098117c9d980d9c0de66
zhanyao <708845433@qq.com>
2017-05-18 09:16:25 +0800

    lookApproveBillController

---
+++ flow-web/src/main/java/com/ecmp/flow/controller/maindata/LookApproveBillController.java
@@ -0,0 +1,54 @@
+package com.ecmp.flow.controller.maindata;
+
+import com.ecmp.config.util.ApiClient;
+import com.ecmp.core.json.JsonUtil;
+import com.ecmp.core.search.PageResult;
+import com.ecmp.core.search.Search;
+import com.ecmp.core.search.SearchUtil;
+import com.ecmp.core.vo.OperateStatus;
+import com.ecmp.flow.api.IDefaultBusinessModelService;
+import com.ecmp.flow.constant.FlowStatus;
+import com.ecmp.flow.entity.DefaultBusinessModel;
+import com.ecmp.vo.OperateResult;
+import com.ecmp.vo.OperateResultWithData;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletRequest;
+import java.text.ParseException;
+
+/**
+ * *************************************************************************************************
+ * <br>
+ * 实现功能：
+ * <br>
+ * ------------------------------------------------------------------------------------------------
+ * <br>
+ * 版本          变更时间             变更人                     变更原因
+ * <br>
+ * ------------------------------------------------------------------------------------------------
+ * <br>
+ * 1.0.00      2017/4/26 9:32      詹耀(xxxlimit)                    新建
+ * <br>
+ * *************************************************************************************************<br>
+ */
+@Controller
+@RequestMapping(value = "/lookApproveBill")
+public class LookApproveBillController {
+
+    @RequestMapping(value = "show", method = RequestMethod.GET)
+    public String show() {
+        return "maindata/LookApproveBillView";
+    }
+
+    @RequestMapping(value = "getApproveBill")
+    @ResponseBody
+    public String getApproveBill(String defBinessModelId) {
+        IDefaultBusinessModelService proxy = ApiClient.createProxy(IDefaultBusinessModelService.class);
+        DefaultBusinessModel result = proxy.findOne(defBinessModelId);
+        return JsonUtil.serialize(result);
+    }
+}
+
