2d4938ed46859de5e087d0c7238b08efc38c2bbb
tanjun <827200436@qq.com>
2017-05-25 13:59:50 +0800

    添加启动人及关联basic用户岗位信息

--- flow-entity/src/main/java/com/ecmp/flow/vo/bpmn/StartEvent.java
+++ flow-entity/src/main/java/com/ecmp/flow/vo/bpmn/StartEvent.java
@@ -1,5 +1,6 @@
 package com.ecmp.flow.vo.bpmn;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 
@@ -18,4 +19,18 @@ import java.io.Serializable;
 @XmlType(name = "startEvent")
 public class StartEvent extends BaseFlowNode implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    /**
+     * 流程启动人
+     */
+    @XmlAttribute(name = "activiti:initiator")
+    private String initiator = "startUserId";
+
+    public String getInitiator() {
+        return initiator;
+    }
+
+    public void setInitiator(String initiator) {
+        this.initiator = initiator;
+    }
 }
