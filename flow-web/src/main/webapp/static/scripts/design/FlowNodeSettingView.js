8f5b07e42421bcbe7557cfd76fedb41fc100a460
yizicangyin@qq.com <yizicangyin@qq.com>
2017-05-26 11:28:30 +0800

    流程设计器

--- flow-web/src/main/webapp/static/scripts/design/FlowNodeSettingView.js
+++ flow-web/src/main/webapp/static/scripts/design/FlowNodeSettingView.js
@@ -40,11 +40,11 @@ EUI.FlowNodeSettingView = EUI.extend(EUI.CustomUI, {
             var id = $(this).attr("id");
             var grid;
             if (userType == "Position") {
-                grid = EUI.getCmp("positionGrid").getGridData();
+                grid = EUI.getCmp("positionGrid");
             } else if (userType == "PositionType") {
-                grid = EUI.getCmp("positionTypeGrid").getGridData();
+                grid = EUI.getCmp("positionTypeGrid");
             } else if (userType == "SelfDefinition") {
-                grid = EUI.getCmp("selfDefGrid").getGridData();
+                grid = EUI.getCmp("selfDefGrid");
             }
             grid.deleteRow(id);
         });
@@ -476,7 +476,7 @@ EUI.FlowNodeSettingView = EUI.extend(EUI.CustomUI, {
                 xtype: "GridPanel",
                 id: "selPositionGrid",
                 gridCfg: {
-                    loadonce: true,
+                    hasPager:false,
                     multiselect: true,
                     url: _ctxPath + "/design/listPos",
                     colModel: this.positionGridColModel()
@@ -495,7 +495,7 @@ EUI.FlowNodeSettingView = EUI.extend(EUI.CustomUI, {
                 selected: true,
                 handler: function () {
                     var data = EUI.getCmp("selPositionTypeGrid").getSelectRow();
-                    EUI.getCmp("positionTypeGrid").setDataInGrid(data);
+                    EUI.getCmp("positionTypeGrid").addRowData(data);
                     win.close();
                 }
             }, {
@@ -508,10 +508,9 @@ EUI.FlowNodeSettingView = EUI.extend(EUI.CustomUI, {
                 xtype: "GridPanel",
                 id: "selPositionTypeGrid",
                 gridCfg: {
-                    loadonce: true,
                     multiselect: true,
                     hasPager: false,
-                    // url: _ctxPath + "/design/listPosType",
+                    url: _ctxPath + "/design/listPosType",
                     colModel: [{
                         name: "id",
                         index: "id",
@@ -524,169 +523,6 @@ EUI.FlowNodeSettingView = EUI.extend(EUI.CustomUI, {
                         label: this.lang.nameText,
                         name: "name",
                         index: "name"
-                    }],
-                    data: [{
-                        "id": "07F93AFF-3B91-11E7-B2C2-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495087975000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495087975000,
-                        "code": "10009",
-                        "name": "技术岗",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "0D953B30-3B91-11E7-B2C2-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495087984000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495087984000,
-                        "code": "10010",
-                        "name": "管理岗",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "13087EB1-3B91-11E7-B2C2-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495087993000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495087993000,
-                        "code": "10011",
-                        "name": "总裁/总经理/CEO",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "2B63799B-3B93-11E7-A16A-9681B6E77C6A",
-                        "createdBy": null,
-                        "createdDate": null,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495090432000,
-                        "code": "10013",
-                        "name": "市场助理",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "32F99D1D-3B93-11E7-A16A-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495088906000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495088906000,
-                        "code": "10014",
-                        "name": "虹信系统管理员",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "337F4A27-3B97-11E7-BF86-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495090625000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495090625000,
-                        "code": "10023",
-                        "name": "程序员/软件工程师",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "340FB1F2-3B91-11E7-B2C2-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495088049000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495088049000,
-                        "code": "10012",
-                        "name": "财务复核",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "3E6EF69E-3B93-11E7-A16A-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495088925000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495088925000,
-                        "code": "10015",
-                        "name": "虹信财务扫描",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "47645B00-3B93-11E7-A16A-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495088940000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495088940000,
-                        "code": "10016",
-                        "name": "虹微高级管理岗",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "4D402451-3B93-11E7-A16A-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495088950000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495088950000,
-                        "code": "10017",
-                        "name": "华润雪花系统管理员",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "5466E432-3B93-11E7-A16A-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495088962000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495088962000,
-                        "code": "10018",
-                        "name": "电商-总经理",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "6DDBD258-3B98-11E7-BF86-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495091152000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495091152000,
-                        "code": "10024",
-                        "name": "系统分析员",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "76028F44-3B93-11E7-A16A-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495089018000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495089018000,
-                        "code": "10019",
-                        "name": "BRM-董事长-长智光电",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "7E36195E-3B95-11E7-A16A-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495089891000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495089891000,
-                        "code": "10022",
-                        "name": "网络工程师",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "7E36A076-3B93-11E7-A16A-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495089032000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495089032000,
-                        "code": "10020",
-                        "name": "虹信系统管理员",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "c0a80171-5bcd-1066-815b-cd85c58f0004",
-                        "createdBy": null,
-                        "createdDate": null,
-                        "lastModifiedBy": "anonymous",
-                        "lastModifiedDate": 1493960842000,
-                        "code": "10005",
-                        "name": "有限公司部门领导(正职)",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "c0a80171-5bcd-1066-815b-cd85ea570005",
-                        "createdBy": "anonymous",
-                        "createdDate": 1493801757000,
-                        "lastModifiedBy": "anonymous",
-                        "lastModifiedDate": 1493801757000,
-                        "code": "10006",
-                        "name": "有限公司部门领导(副职)",
-                        "tenantCode": "10011"
-                    }, {
-                        "id": "FF8E896E-3B90-11E7-B2C2-9681B6E77C6A",
-                        "createdBy": "10011,anonymous[anonymous]",
-                        "createdDate": 1495087961000,
-                        "lastModifiedBy": "10011,anonymous[anonymous]",
-                        "lastModifiedDate": 1495087961000,
-                        "code": "10008",
-                        "name": "SPM-HX-中心副总监",
-                        "tenantCode": "10011"
                     }]
                 }
             }]
