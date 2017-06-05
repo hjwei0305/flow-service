c9d71b3e200525337708d605eb30aaad9b46ad6c
zhanyao <708845433@qq.com>
2017-06-05 16:30:04 +0800

    FlowHistoryService

--- flow-api/src/main/java/com/ecmp/flow/api/IFlowHistoryService.java
+++ flow-api/src/main/java/com/ecmp/flow/api/IFlowHistoryService.java
@@ -13,6 +13,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 /**
  * *************************************************************************************************
@@ -54,4 +55,15 @@ public interface IFlowHistoryService extends IBaseService<FlowHistory, String> {
     @ApiOperation(value = "获取分页数据", notes = "测试 获取分页数据")
     PageResult<FlowHistory> findByPage(Search searchConfig);
 
+    /**
+     * 根据流程实例ID查询流程历史
+     * @param instanceId
+     * @return
+     */
+    @POST
+    @Path("findByInstanceId")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "查询流程历史", notes = "查询流程历史")
+    List<FlowHistory> findByInstanceId(String instanceId);
 }
