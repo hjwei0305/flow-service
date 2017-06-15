4c68e59d9e0bb7f32792d14493d9d034ec91333a
tanjun <827200436@qq.com>
2017-06-15 21:22:14 +0800

    待办查询优化

---
+++ flow-dao/src/main/java/com/ecmp/flow/dao/CustomFlowTaskDao.java
@@ -0,0 +1,26 @@
+package com.ecmp.flow.dao;
+
+import com.ecmp.core.search.PageResult;
+import com.ecmp.core.search.Search;
+import com.ecmp.flow.entity.FlowTask;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * *************************************************************************************************
+ * <p/>
+ * 实现功能：
+ * <p>
+ * ------------------------------------------------------------------------------------------------
+ * 版本          变更时间             变更人                     变更原因
+ * ------------------------------------------------------------------------------------------------
+ * 1.0.00      2017/6/15 20:32      谭军(tanjun)                    新建
+ * <p/>
+ * *************************************************************************************************
+ */
+public interface CustomFlowTaskDao {
+    /**
+     * 通过业务实体类型id,基于动态组合条件对象和分页(含排序)对象查询数据集合
+     */
+    @Transactional(readOnly = true)
+    public PageResult<FlowTask> findByPageByBusinessModelId(String businessModelId,String executorAccount, Search searchConfig);
+}
---
+++ flow-dao/src/main/java/com/ecmp/flow/dao/impl/FlowTaskDaoImpl.java
@@ -0,0 +1,59 @@
+package com.ecmp.flow.dao.impl;
+
+import com.ecmp.core.dao.impl.BaseEntityDaoImpl;
+import com.ecmp.core.search.PageInfo;
+import com.ecmp.core.search.PageResult;
+import com.ecmp.core.search.Search;
+import com.ecmp.core.search.SearchFilter;
+import com.ecmp.flow.dao.CustomFlowTaskDao;
+import com.ecmp.flow.entity.FlowTask;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * *************************************************************************************************
+ * <br>
+ * 实现功能：
+ * <br>
+ * ------------------------------------------------------------------------------------------------
+ * <br>
+ * 版本          变更时间             变更人                     变更原因
+ * <br>
+ * ------------------------------------------------------------------------------------------------
+ * <br>
+ * 1.0.00      2017/5/9 20:26      马超(Vision.Mac)                新建
+ * <br>
+ * *************************************************************************************************
+ */
+public class FlowTaskDaoImpl extends BaseEntityDaoImpl<FlowTask> implements CustomFlowTaskDao {
+    public FlowTaskDaoImpl(EntityManager entityManager) {
+        super(FlowTask.class, entityManager);
+    }
+
+    public PageResult<FlowTask> findByPageByBusinessModelId(String businessModelId,String executorAccount, Search searchConfig) {
+        PageInfo pageInfo = searchConfig.getPageInfo();
+
+        TypedQuery<Integer> queryTotal = entityManager.createQuery("select count(ft.id) from com.ecmp.flow.entity.FlowTask ft where ft.executorAccount  = :executorAccount and ft.flowDefinitionId in(select fd.id from FlowDefination fd where fd.id in(select fType.id from FlowType fType where fType.id in( select bm.id from BusinessModel bm where bm.id = :businessModelId)) ) ", Integer.class);
+        queryTotal.setParameter("executorAccount",executorAccount);
+        queryTotal.setParameter("businessModelId",businessModelId);
+        Integer total = queryTotal.getSingleResult();
+
+        TypedQuery<FlowTask> query = entityManager.createQuery("select ft from com.ecmp.flow.entity.FlowTask ft where ft.executorAccount  = :executorAccount and ft.flowDefinitionId in(select fd.id from FlowDefination fd where fd.id in(select fType.id from FlowType fType where fType.id in( select bm.id from BusinessModel bm where bm.id = :businessModelId)) ) order by ft.lastEditedDate desc", FlowTask.class);
+        query.setParameter("executorAccount",executorAccount);
+        query.setParameter("businessModelId",businessModelId);
+        query.setFirstResult( (pageInfo.getPage()-1) * pageInfo.getRows() );
+        query.setMaxResults( pageInfo.getRows() );
+        List<FlowTask>  result = query.getResultList();
+
+        PageResult<FlowTask> pageResult = new PageResult<>();
+        pageResult.setPage(pageInfo.getPage());
+        pageResult.setRows(result);
+        pageResult.setRecords(result.size());
+        pageResult.setTotal(total);
+
+        return pageResult;
+    }
+}
