81676e5045a4cbf8ecffc5e1564b7d007e8887a9
yizicangyin@qq.com <yizicangyin@qq.com>
2017-06-16 13:53:46 +0800

    去掉logger

--- flow-entity/src/main/java/com/ecmp/flow/entity/FlowDefVersion.java
+++ flow-entity/src/main/java/com/ecmp/flow/entity/FlowDefVersion.java
@@ -1,15 +1,12 @@
 package com.ecmp.flow.entity;
 
-import java.util.HashSet;
-import java.util.Set;
-import javax.persistence.*;
-
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.hibernate.annotations.GenericGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * *************************************************************************************************
@@ -26,295 +23,290 @@ import org.slf4j.LoggerFactory;
  */
 @Entity
 @Table(name = "flow_def_version", catalog = "ecmp_flow", uniqueConstraints = @UniqueConstraint(columnNames = "def_key"))
-public class FlowDefVersion extends com.ecmp.core.entity.BaseAuditableEntity implements Cloneable{
+public class FlowDefVersion extends com.ecmp.core.entity.BaseAuditableEntity implements Cloneable {
 
-	@Transient
-	private final Logger logger = LoggerFactory.getLogger(FlowDefVersion.class);
+    /**
+     * 乐观锁-版本
+     */
+    //@Version
+    @Column(name = "version")
+    private Integer version = 0;
 
-
-	/**
-	 * 乐观锁-版本
-	 */
-	//@Version
-	@Column(name = "version")
-	private Integer version = 0;
-
-	/**
-	 * 所属流程定义
-	 */
-	@ManyToOne()
-	@JoinColumn(name = "flow_defination_id")
-	private FlowDefination flowDefination;
+    /**
+     * 所属流程定义
+     */
+    @ManyToOne()
+    @JoinColumn(name = "flow_defination_id")
+    private FlowDefination flowDefination;
 
 
-	/**
-	 * 定义ID
-	 */
+    /**
+     * 定义ID
+     */
 
-	@Column(name = "act_def_id", length = 36)
-	private String actDefId;
+    @Column(name = "act_def_id", length = 36)
+    private String actDefId;
 
-	/**
-	 * 定义KEY
-	 */
-	@Column(name = "def_key",nullable = false)
-	private String defKey;
+    /**
+     * 定义KEY
+     */
+    @Column(name = "def_key", nullable = false)
+    private String defKey;
 
-	/**
-	 * 名称
-	 */
-	@Column(name = "name", nullable = false, length = 80)
-	private String name;
+    /**
+     * 名称
+     */
+    @Column(name = "name", nullable = false, length = 80)
+    private String name;
 
-	/**
-	 * 部署ID
-	 */
-	@Column(name = "act_deploy_id", length = 36)
-	private String actDeployId;
+    /**
+     * 部署ID
+     */
+    @Column(name = "act_deploy_id", length = 36)
+    private String actDeployId;
 
-	/**
-	 * 启动条件UEL
-	 */
-	@Column(name = "start_uel")
-	private String startUel;
+    /**
+     * 启动条件UEL
+     */
+    @Column(name = "start_uel")
+    private String startUel;
 
-	/**
-	 * 版本号
-	 */
-	@Column(name = "version_code")
-	private Integer versionCode;
+    /**
+     * 版本号
+     */
+    @Column(name = "version_code")
+    private Integer versionCode;
 
-	/**
-	 * 优先级
-	 */
-	@Column(name = "priority")
-	private Integer priority;
+    /**
+     * 优先级
+     */
+    @Column(name = "priority")
+    private Integer priority;
 
-	/**
-	 * 流程JSON文本
-	 */
-	@Lob
-	@Basic(fetch=FetchType.LAZY)
-	@Column(name = "def_json", length = 65535)
-	private String defJson;
+    /**
+     * 流程JSON文本
+     */
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "def_json", length = 65535)
+    private String defJson;
 
-	/**
-	 * 流程BPMN文本
-	 */
-	@Lob
-	@Basic(fetch=FetchType.LAZY)
-	@Column(name = "def_bpmn", length = 65535)
-	private String defBpmn;
+    /**
+     * 流程BPMN文本
+     */
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "def_bpmn", length = 65535)
+    private String defBpmn;
 
-	/**
-	 * 最终定义XML
-	 */
-	@Lob
-	@Basic(fetch=FetchType.LAZY)
-	@Column(name = "def_xml", length = 65535)
-	private String defXml;
+    /**
+     * 最终定义XML
+     */
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "def_xml", length = 65535)
+    private String defXml;
 
-	/**
-	 * 描述
-	 */
-	@Column(name = "depict")
-	private String depict;
+    /**
+     * 描述
+     */
+    @Column(name = "depict")
+    private String depict;
 
-	/**
-	 * 拥有的流程实例
-	 */
-	@Transient
+    /**
+     * 拥有的流程实例
+     */
+    @Transient
 //	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "flowDefVersion")
-	private Set<FlowInstance> flowInstances = new HashSet<FlowInstance>(0);
+    private Set<FlowInstance> flowInstances = new HashSet<FlowInstance>(0);
 
-	// Constructors
+    // Constructors
 
-	/** default constructor */
-	public FlowDefVersion() {
-	}
+    /**
+     * default constructor
+     */
+    public FlowDefVersion() {
+    }
 
-	/** minimal constructor */
-	public FlowDefVersion(String defKey, String name) {
-		this.defKey = defKey;
-		this.name = name;
+    /**
+     * minimal constructor
+     */
+    public FlowDefVersion(String defKey, String name) {
+        this.defKey = defKey;
+        this.name = name;
 
-	}
+    }
 
-	/** full constructor */
-	public FlowDefVersion(FlowDefination flowDefination, String defKey,
-			String name, String actDeployId, String startUel,
-			Integer versionCode, Integer priority, String defJson,
-			String defBpmn, String defXml, String depict, Set<FlowInstance> flowInstances) {
-		this.flowDefination = flowDefination;
-		this.defKey = defKey;
-		this.name = name;
-		this.actDeployId = actDeployId;
-		this.startUel = startUel;
-		this.versionCode = versionCode;
-		this.priority = priority;
-		this.defJson = defJson;
-		this.defBpmn = defBpmn;
-		this.defXml = defXml;
-		this.depict = depict;
-		this.flowInstances = flowInstances;
-	}
-
-
-
-
-	public FlowDefination getFlowDefination() {
-		return this.flowDefination;
-	}
-
-	public void setFlowDefination(FlowDefination flowDefination) {
-		this.flowDefination = flowDefination;
-	}
-
-	public String getActDefId() {
-		return actDefId;
-	}
-
-	public void setActDefId(String actDefId) {
-		this.actDefId = actDefId;
-	}
-
-	public String getDefKey() {
-		return this.defKey;
-	}
-
-	public void setDefKey(String defKey) {
-		this.defKey = defKey;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getActDeployId() {
-		return this.actDeployId;
-	}
-
-	public void setActDeployId(String actDeployId) {
-		this.actDeployId = actDeployId;
-	}
-
-	public String getStartUel() {
-		return this.startUel;
-	}
-
-	public void setStartUel(String startUel) {
-		this.startUel = startUel;
-	}
-
-	public Integer getVersionCode() {
-		return this.versionCode;
-	}
-
-	public void setVersionCode(Integer versionCode) {
-		this.versionCode = versionCode;
-	}
-
-	public Integer getPriority() {
-		return this.priority;
-	}
-
-	public void setPriority(Integer priority) {
-		this.priority = priority;
-	}
-
-	public String getDefJson() {
-		return this.defJson;
-	}
-
-	public void setDefJson(String defJson) {
-		this.defJson = defJson;
-	}
-
-	public String getDefBpmn() {
-		return this.defBpmn;
-	}
-
-	public void setDefBpmn(String defBpmn) {
-		this.defBpmn = defBpmn;
-	}
-
-	public String getDefXml() {
-		return this.defXml;
-	}
-
-	public void setDefXml(String defXml) {
-		this.defXml = defXml;
-	}
-
-	public String getDepict() {
-		return this.depict;
-	}
-
-	public void setDepict(String depict) {
-		this.depict = depict;
-	}
-
-	public Set<FlowInstance> getFlowInstances() {
-		return this.flowInstances;
-	}
-
-	public void setFlowInstances(Set<FlowInstance> flowInstances) {
-		this.flowInstances = flowInstances;
-	}
-
-	public Integer getVersion() {
-		return version;
-	}
-
-	public void setVersion(Integer version) {
-		this.version = version;
-	}
+    /**
+     * full constructor
+     */
+    public FlowDefVersion(FlowDefination flowDefination, String defKey,
+                          String name, String actDeployId, String startUel,
+                          Integer versionCode, Integer priority, String defJson,
+                          String defBpmn, String defXml, String depict, Set<FlowInstance> flowInstances) {
+        this.flowDefination = flowDefination;
+        this.defKey = defKey;
+        this.name = name;
+        this.actDeployId = actDeployId;
+        this.startUel = startUel;
+        this.versionCode = versionCode;
+        this.priority = priority;
+        this.defJson = defJson;
+        this.defBpmn = defBpmn;
+        this.defXml = defXml;
+        this.depict = depict;
+        this.flowInstances = flowInstances;
+    }
 
 
+    public FlowDefination getFlowDefination() {
+        return this.flowDefination;
+    }
 
-	@Override
-	public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
-	}
+    public void setFlowDefination(FlowDefination flowDefination) {
+        this.flowDefination = flowDefination;
+    }
 
-	@Override
-	public String toString() {
+    public String getActDefId() {
+        return actDefId;
+    }
 
-		return new ToStringBuilder(this)
-				.append("id", this.getId())
-				.append("flowDefination", flowDefination)
-				.append("defKey", defKey)
-				.append("name", name)
-				.append("actDeployId", actDeployId)
-				.append("startUel", startUel)
-				.append("versionCode", versionCode)
-				.append("priority", priority)
-				.append("defJson", defJson)
-				.append("defBpmn", defBpmn)
-				.append("defXml", defXml)
-				.append("depict", depict)
-				.append("flowInstances", flowInstances)
-				.toString();
-	}
+    public void setActDefId(String actDefId) {
+        this.actDefId = actDefId;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		return EqualsBuilder.reflectionEquals(this, obj);
-	}
+    public String getDefKey() {
+        return this.defKey;
+    }
 
-	@Override
-	public Object clone() {
-		FlowDefination o = null;
-		try {
-			// Object中的clone()识别出你要复制的是哪一个对象。
-			o = (FlowDefination) super.clone();
-		} catch (CloneNotSupportedException e) {
-			logger.error(e.getMessage());
-		}
-		return o;
-	}
+    public void setDefKey(String defKey) {
+        this.defKey = defKey;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getActDeployId() {
+        return this.actDeployId;
+    }
+
+    public void setActDeployId(String actDeployId) {
+        this.actDeployId = actDeployId;
+    }
+
+    public String getStartUel() {
+        return this.startUel;
+    }
+
+    public void setStartUel(String startUel) {
+        this.startUel = startUel;
+    }
+
+    public Integer getVersionCode() {
+        return this.versionCode;
+    }
+
+    public void setVersionCode(Integer versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public Integer getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getDefJson() {
+        return this.defJson;
+    }
+
+    public void setDefJson(String defJson) {
+        this.defJson = defJson;
+    }
+
+    public String getDefBpmn() {
+        return this.defBpmn;
+    }
+
+    public void setDefBpmn(String defBpmn) {
+        this.defBpmn = defBpmn;
+    }
+
+    public String getDefXml() {
+        return this.defXml;
+    }
+
+    public void setDefXml(String defXml) {
+        this.defXml = defXml;
+    }
+
+    public String getDepict() {
+        return this.depict;
+    }
+
+    public void setDepict(String depict) {
+        this.depict = depict;
+    }
+
+    public Set<FlowInstance> getFlowInstances() {
+        return this.flowInstances;
+    }
+
+    public void setFlowInstances(Set<FlowInstance> flowInstances) {
+        this.flowInstances = flowInstances;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+
+        return new ToStringBuilder(this)
+                .append("id", this.getId())
+                .append("flowDefination", flowDefination)
+                .append("defKey", defKey)
+                .append("name", name)
+                .append("actDeployId", actDeployId)
+                .append("startUel", startUel)
+                .append("versionCode", versionCode)
+                .append("priority", priority)
+                .append("defJson", defJson)
+                .append("defBpmn", defBpmn)
+                .append("defXml", defXml)
+                .append("depict", depict)
+                .append("flowInstances", flowInstances)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        FlowDefination o = null;
+        // Object中的clone()识别出你要复制的是哪一个对象。
+        o = (FlowDefination) super.clone();
+        return o;
+    }
 }
