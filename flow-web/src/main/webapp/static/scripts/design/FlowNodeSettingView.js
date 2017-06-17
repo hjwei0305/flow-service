a79b0e25f544a71d0febb3b2e4ca4651e11d1913
yizicangyin@qq.com <yizicangyin@qq.com>
2017-06-17 15:22:39 +0800

    流程设计器

--- flow-web/src/main/webapp/static/scripts/design/FlowNodeSettingView.js
+++ flow-web/src/main/webapp/static/scripts/design/FlowNodeSettingView.js
@@ -519,8 +519,6 @@ EUI.FlowNodeSettingView = EUI.extend(EUI.CustomUI, {
             hidden: true,
             id: "positionTypeGrid",
             gridCfg: {
-                width:400,
-                autowidth:false,
                 loadonce: true,
                 hasPager: false,
                 // url: _ctxPath + "",
