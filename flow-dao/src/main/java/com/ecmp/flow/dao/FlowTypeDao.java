db5787523a0e57608bcfcacf6d8cc76331e3b508
zhanyao <708845433@qq.com>
2017-07-06 13:41:27 +0800

    dao

--- flow-dao/src/main/java/com/ecmp/flow/dao/FlowTypeDao.java
+++ flow-dao/src/main/java/com/ecmp/flow/dao/FlowTypeDao.java
@@ -5,7 +5,14 @@ import com.ecmp.core.dao.jpa.BaseDao;
 import com.ecmp.flow.entity.FlowType;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FlowTypeDao extends BaseEntityDao<FlowType> {
 
+    /**
+     * 根据业务实体的id来查询流程类型
+     *
+     */
+    List<FlowType> findByBusinessModelId(String businessModelId);
 }
