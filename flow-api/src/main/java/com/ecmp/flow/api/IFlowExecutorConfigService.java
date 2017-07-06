5d3932488998e262b39c277a285179f7cedaded6
chenshuang <c_sjiayou@163.com>
2017-07-06 11:03:42 +0800

    添加自定义执行人后台接口方法及控制器

---
+++ flow-api/src/main/java/com/ecmp/flow/api/IFlowExecutorConfigService.java
@@ -0,0 +1,57 @@
+package com.ecmp.flow.api;
+
+import com.ecmp.core.search.PageResult;
+import com.ecmp.core.search.Search;
+import com.ecmp.flow.api.common.api.IBaseService;
+import com.ecmp.flow.entity.FlowExecutorConfig;
+import com.ecmp.vo.OperateResultWithData;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * *************************************************************************************************
+ * <p/>
+ * 实现功能：流程自定义执行人配置API接口定义
+ * <p>
+ * ------------------------------------------------------------------------------------------------
+ * 版本          变更时间             变更人                     变更原因
+ * ------------------------------------------------------------------------------------------------
+ * 1.0.00      2017/7/5 9:30        陈爽(chenshuang)               新建
+ * <p/>
+ * *************************************************************************************************
+ */
+@Path("flowExecutorConfig")
+@Api(value = "IFlowExecutorConfigService 自定义执行人配置的API接口")
+public interface IFlowExecutorConfigService extends IBaseService<FlowExecutorConfig, String> {
+
+    /**
+     * 保存一个实体
+     * @param flowExecutorConfig 实体
+     * @return 保存后的实体
+     */
+    @POST
+    @Path("save")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "保存实体",notes = "测试 保存实体")
+    OperateResultWithData<FlowExecutorConfig> save(FlowExecutorConfig flowExecutorConfig);
+
+    /**
+     * 获取分页数据
+     *
+     * @return 实体清单
+     */
+    @POST
+    @Path("findByPage")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "获取分页数据", notes = "测试 获取分页数据")
+    PageResult<FlowExecutorConfig> findByPage(Search searchConfig);
+
+}
---
+++ flow-service/src/main/java/com/ecmp/flow/service/FlowExecutorConfigService.java
@@ -0,0 +1,38 @@
+package com.ecmp.flow.service;
+
+import com.ecmp.core.dao.BaseEntityDao;
+import com.ecmp.core.service.BaseEntityService;
+import com.ecmp.flow.api.IFlowExecutorConfigService;
+import com.ecmp.flow.api.IFlowServiceUrlService;
+import com.ecmp.flow.dao.BusinessModelDao;
+import com.ecmp.flow.dao.FlowExecutorConfigDao;
+import com.ecmp.flow.entity.FlowExecutorConfig;
+import com.ecmp.flow.entity.FlowServiceUrl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * *************************************************************************************************
+ * <p/>
+ * 实现功能：流程自定义执行人配置API接口实现
+ * <p>
+ * ------------------------------------------------------------------------------------------------
+ * 版本          变更时间             变更人                     变更原因
+ * ------------------------------------------------------------------------------------------------
+ * 1.0.00      2017/7/5 9:50      陈爽(chenshuang)               新建
+ * <p/>
+ * *************************************************************************************************
+ */
+@Service
+public class FlowExecutorConfigService extends BaseEntityService<FlowExecutorConfig> implements IFlowExecutorConfigService {
+
+    @Autowired
+    private FlowExecutorConfigDao flowExecutorConfigDao;
+
+    @Override
+    protected BaseEntityDao<FlowExecutorConfig> getDao() {
+        return this.flowExecutorConfigDao;
+    }
+}
