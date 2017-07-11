627bde7c5297cdb5aa950ca0f67de75e9a6e8f58
xlx <752085188@qq.com>
2017-07-11 16:25:17 +0800

    approve

--- flow-web/src/main/webapp/static/scripts/approve/FlowApprove.js
+++ flow-web/src/main/webapp/static/scripts/approve/FlowApprove.js
@@ -267,7 +267,7 @@ Flow.flow.FlowApprove = EUI.extend(EUI.CustomUI, {
             var item = data[i];
             if (item.flowTaskType == "CounterSign") {
                 html += '<div class="flow-decision-item" id="' + item.id + '" type="' + item.type.toLowerCase() + '">' +
-                    '<div class="excutor-item-title"><div>' + item.name + '-[会签任务]</div></div></div>';
+                    '<div class="excutor-item-title"><div class="flow-countersign">' + item.name + '-【会签任务】</div></div></div>';
                 g.initDealCheckBox();
                 this.desionType = 2;
             } else {
