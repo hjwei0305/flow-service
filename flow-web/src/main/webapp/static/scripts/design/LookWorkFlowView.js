d06046dc667d5f0ce9ee6eaa4331d17644f311f3
张洁 <jie3.zhang@changhong.com>
2017-07-18 16:43:11 +0800

    流程定义编辑、查看-会签任务显示图标

--- flow-web/src/main/webapp/static/scripts/design/LookWorkFlowView.js
+++ flow-web/src/main/webapp/static/scripts/design/LookWorkFlowView.js
@@ -339,17 +339,31 @@ EUI.LookWorkFlowView = EUI.extend(EUI.CustomUI, {
         }
         var css = node.css;
         if (!css) {
-            if (node.nodeType == "Normal") {
-                css = "usertask";
-            } else if (node.nodeType == "SingleSign") {
-                css = "singletask";
-            }  else {
-                css = "countertask";
-                if (node.nodeConfig.normal.isSequential) {
-                    css = "countertask serial-countertask";
-                } else {
-                    css = "countertask parallel-countertask";
-                }
+            switch (node.nodeType){
+                case "Normal":
+                    css = "usertask";
+                    break;
+                case "SingleSign":
+                    css = "singletask";
+                    break;
+                case "CounterSign":
+                    css = "countertask";
+                    node.nodeConfig.normal.isSequential=node.nodeConfig.normal.isSequential.toString();
+                    if (node.nodeConfig.normal.isSequential=="true") {
+                        css = "countertask serial-countertask";
+                    } else {
+                        css = "countertask parallel-countertask";
+                    }
+                    break;
+                case "Approve":
+                    css = "approvetask";
+                    break;
+                case "ParallelTask":
+                    css = "paralleltask";
+                    break;
+                case "SerialTask":
+                    css = "serialtask";
+                    break;
             }
         }
         return "<div tabindex=0 id='" + id
