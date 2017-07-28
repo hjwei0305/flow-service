00dfa52df92ce40b3f20ae2b9f10a950553e4dee
张洁 <jie3.zhang@changhong.com>
2017-07-28 16:32:32 +0800

    流程定义-修改开始任务后禁止连接人工网关"

--- flow-web/src/main/webapp/static/scripts/design/WorkFlowView.js
+++ flow-web/src/main/webapp/static/scripts/design/WorkFlowView.js
@@ -803,7 +803,7 @@ EUI.WorkFlowView = EUI.extend(EUI.CustomUI, {
                     }
                     var type = $("#" + connection.sourceId).attr("type");
                     var busType = $("#" + connection.targetId).attr("bustype");
-                    if(type="StartEvent"&&busType=="ManualExclusiveGateway") {
+                    if(type=="StartEvent"&&busType=="ManualExclusiveGateway") {
                         EUI.ProcessStatus({
                             success: false,
                             msg: "开始任务后禁止连接人工网关"
