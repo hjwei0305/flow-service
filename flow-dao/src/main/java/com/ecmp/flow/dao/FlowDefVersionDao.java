e3262f8b9a93a99d6107c7ce8628ffbd34e7cdef
tanjun <827200436@qq.com>
2018-01-25 11:06:24 +0800

    dao-缓存清除

--- flow-dao/src/main/java/com/ecmp/flow/dao/FlowDefVersionDao.java
+++ flow-dao/src/main/java/com/ecmp/flow/dao/FlowDefVersionDao.java
@@ -12,25 +12,25 @@ import java.util.List;
 @Repository
 public interface FlowDefVersionDao extends BaseEntityDao<FlowDefVersion> {
 
-    @Cacheable(cacheNames="findByDefIdAndVersionCode")
+//    @Cacheable(cacheNames="findByDefIdAndVersionCode")
     @Query("select fv from com.ecmp.flow.entity.FlowDefVersion fv where fv.flowDefination.id  = :defId and fv.versionCode = :versionCode")
     public FlowDefVersion findByDefIdAndVersionCode(@Param("defId")String defId,@Param("versionCode")Integer versionCode);
 
-    @Cacheable(cacheNames="findByFlowDefinationId")
+//    @Cacheable(cacheNames="findByFlowDefinationId")
     public List<FlowDefVersion> findByFlowDefinationId(String defId);
 
-    @Cacheable(cacheNames="findByFlowDefinationIdActivate")
+//    @Cacheable(cacheNames="findByFlowDefinationIdActivate")
     @Query("select fv from com.ecmp.flow.entity.FlowDefVersion fv where fv.flowDefination.id  = :defId and fv.flowDefinationStatus = 1 order by fv.versionCode desc")
     public List<FlowDefVersion> findByFlowDefinationIdActivate(@Param("defId")String defId);
 
-    @Cacheable(cacheNames="findByKeyActivate")
+//    @Cacheable(cacheNames="findByKeyActivate")
     @Query("select fv from com.ecmp.flow.entity.FlowDefVersion fv where fv.defKey  = :defKey and fv.flowDefinationStatus = 1 order by fv.versionCode desc")
     public List<FlowDefVersion> findByKeyActivate(@Param("defKey")String defKey);
 
-    @Cacheable(cacheNames="findByActDefId")
+//    @Cacheable(cacheNames="findByActDefId")
     public FlowDefVersion findByActDefId(String actDefId);
 
-    @Cacheable(cacheNames="findByKeyAndVersionCode")
+//    @Cacheable(cacheNames="findByKeyAndVersionCode")
     @Query("select fv from com.ecmp.flow.entity.FlowDefVersion fv where fv.defKey  = :defKey and fv.versionCode = :versionCode")
     public FlowDefVersion findByKeyAndVersionCode(@Param("defKey")String defKey,@Param("versionCode")Integer versionCode);
 
--- flow-dao/src/main/java/com/ecmp/flow/dao/FlowDefinationDao.java
+++ flow-dao/src/main/java/com/ecmp/flow/dao/FlowDefinationDao.java
@@ -11,18 +11,18 @@ import java.util.List;
 
 @Repository
 public interface FlowDefinationDao extends BaseEntityDao<FlowDefination> {
-    @Cacheable(cacheNames="findByDefKey")
+//    @Cacheable(cacheNames="findByDefKey")
     public FlowDefination findByDefKey(String defKey);
 
-    @Cacheable(cacheNames="findByTypeCode")
+//    @Cacheable(cacheNames="findByTypeCode")
     @Query("select f from com.ecmp.flow.entity.FlowDefination f where f.flowType.id = (select ft.id from com.ecmp.flow.entity.FlowType ft where ft.code  = :typeCode)  order by f.priority desc,f.lastEditedDate desc")
     public List<FlowDefination> findByTypeCode(@Param("typeCode")String typeCode);
 
-    @Cacheable(cacheNames="findByTypeCodeAndOrgId")
+//    @Cacheable(cacheNames="findByTypeCodeAndOrgId")
     @Query("select f from com.ecmp.flow.entity.FlowDefination f where f.flowType.id = (select ft.id from com.ecmp.flow.entity.FlowType ft where ft.code  = :typeCode) and f.orgId = :orgId  order by f.priority desc,f.lastEditedDate desc")
     public List<FlowDefination> findByTypeCodeAndOrgId(@Param("typeCode")String typeCode,@Param("orgId")String orgId);
 
-    @Cacheable(cacheNames="findByTypeCodeAndOrgCode")
+//    @Cacheable(cacheNames="findByTypeCodeAndOrgCode")
     @Query("select f from com.ecmp.flow.entity.FlowDefination f where f.flowDefinationStatus =1 and f.flowType.id = (select ft.id from com.ecmp.flow.entity.FlowType ft where ft.code  = :typeCode) and f.orgCode = :orgCode  order by f.priority desc,f.lastEditedDate desc")
     public List<FlowDefination> findByTypeCodeAndOrgCode(@Param("typeCode")String typeCode,@Param("orgCode")String orgCode);
 }
