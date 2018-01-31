99fe6462c9737a7768f8b54e95e09288d5d19199
tanjun <827200436@qq.com>
2018-01-31 08:27:57 +0800

    压测试-启动-优化

--- flow-dao/src/main/java/com/ecmp/flow/dao/FlowInstanceDao.java
+++ flow-dao/src/main/java/com/ecmp/flow/dao/FlowInstanceDao.java
@@ -4,21 +4,25 @@ import com.ecmp.core.dao.BaseEntityDao;
 import com.ecmp.core.dao.jpa.BaseDao;
 import com.ecmp.flow.entity.FlowInstance;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.QueryHint;
 import java.util.List;
 
 @Repository
 public interface FlowInstanceDao extends BaseEntityDao<FlowInstance> {
 
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
     @Query("select ft from com.ecmp.flow.entity.FlowInstance ft where ft.actInstanceId = :actInstanceId")
     public  FlowInstance findByActInstanceId(@Param("actInstanceId")String actInstanceId);
-
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
     public  List<FlowInstance> findByBusinessId(String businessId);
-
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
     public   List<FlowInstance> findByParentId(String parentId);
 
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
     @Query("select ft from com.ecmp.flow.entity.FlowInstance ft where ft.businessId = :businessId  order by ft.lastEditedDate desc")
     public List<FlowInstance> findByBusinessIdOrder(@Param("businessId")String businessId);
 
@@ -28,9 +32,10 @@ public interface FlowInstanceDao extends BaseEntityDao<FlowInstance> {
      * @param creatorId
      * @return
      */
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
     @Query("select ft from com.ecmp.flow.entity.FlowInstance ft where ft.creatorId  = :creatorId and ft.ended = :ended ")
     public List<FlowInstance> findByCreatorId(@Param("creatorId")String creatorId,@Param("ended")Boolean ended);
-
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
     public  List<FlowInstance> findByFlowDefVersionId(String flowDefVersionId);
 
 
