23c7151dd2431dc38134cfaf4fc223d5e997e3b6
冯华 <hua.feng@changhong.com>
2018-04-25 20:05:45 +0800

    Revert "权限问题-覆盖"
    This reverts commit 75c81967f984fa74b6d8f43d7e64f2252c51be5c

--- flow-web/src/main/java/com/ecmp/flow/controller/maindata/AppModuleController.java
+++ flow-web/src/main/java/com/ecmp/flow/controller/maindata/AppModuleController.java
@@ -1,6 +1,5 @@
 package com.ecmp.flow.controller.maindata;
 
-import com.ecmp.annotation.IgnoreCheckAuth;
 import com.ecmp.config.util.ApiClient;
 import com.ecmp.core.vo.OperateStatus;
 import com.ecmp.flow.api.IAppModuleService;
@@ -34,7 +33,6 @@ import java.util.List;
  */
 @Controller
 @RequestMapping(value = "/appModule")
-@IgnoreCheckAuth
 public class AppModuleController {
 
     @RequestMapping(value = "show", method = RequestMethod.GET)
