39211cc53fbf6f98d501f854f2dacb4a3b3fb1e0
tanjun <827200436@qq.com>
2018-06-12 11:31:39 +0800

    权限过滤2

--- flow-api/src/main/java/com/ecmp/flow/api/IAppModuleService.java
+++ flow-api/src/main/java/com/ecmp/flow/api/IAppModuleService.java
@@ -4,10 +4,9 @@ import com.ecmp.core.api.IBaseEntityService;
 import com.ecmp.core.api.IFindAllService;
 import com.ecmp.flow.entity.AppModule;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -33,5 +32,15 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public interface IAppModuleService extends IBaseEntityService<AppModule>,IFindAllService<AppModule> {
+    /**
+     * 获取分页数据
+     *
+     * @return 实体清单
+     */
+    @GET
+    @Path("findAllByAuth")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "通过当前用户筛选有权限的数据", notes = "通过当前用户筛选有权限的数据")
     public List<AppModule> findAllByAuth();
 }
