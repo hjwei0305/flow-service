8421b38adbcf9e7348909f484f3206e37ba07bf3
马超 <chao2.ma@changhong.com>
2018-08-21 14:20:17 +0800

    升级版本，解决依赖不一致问题

---
+++ flow-service/src/test/java/com/ecmp/flow/service/FlowTypeServiceTest.java
@@ -0,0 +1,39 @@
+package com.ecmp.flow.service;
+
+import com.ecmp.flow.BasicContextTestCase;
+import com.ecmp.flow.entity.BusinessModel;
+import com.ecmp.flow.entity.FlowType;
+import com.ecmp.vo.OperateResultWithData;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author 马超(Vision.Mac)
+ * @version 1.0.1 2018/6/14 9:51
+ */
+public class FlowTypeServiceTest extends BasicContextTestCase {
+
+    @Autowired
+    private BusinessModelService businessModelService;
+
+    @Autowired
+    private FlowTypeService flowTypeService;
+
+    @Test
+    public void save() {
+        List<BusinessModel> businessModels = businessModelService.findAll();
+        if (businessModels != null && businessModels.size() > 0) {
+            FlowType flowType = new FlowType();
+            flowType.setBusinessModel(businessModels.get(0));
+            flowType.setCode("test_mac");
+            flowType.setName("测试");
+            flowType.setDepict("测试");
+            OperateResultWithData<FlowType> op = flowTypeService.save(flowType);
+            System.out.println(op);
+        }
+    }
+}
