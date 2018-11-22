d0530d394ced16719cbc788198144023ea41a20a
tanjun <827200436@qq.com>
2018-11-22 14:07:49 +0800

    合并dto

--- flow-entity/src/main/java/com/ecmp/flow/dto/FlowTaskExecutorIdAndCount.java
+++ flow-entity/src/main/java/com/ecmp/flow/dto/FlowTaskExecutorIdAndCount.java
@@ -16,12 +16,36 @@ import java.io.Serializable;
  */
 public class FlowTaskExecutorIdAndCount implements Serializable {
     private String executorId;
+    private String executorName;
+    private String flowDefinitionId;
+    private String flowName;
+    private String taskKey;
+    private String taskName;
     private long count;
 
     public FlowTaskExecutorIdAndCount(){}
 
     public FlowTaskExecutorIdAndCount(String executorId,long count){
         this.executorId = executorId;
+        this.count = count;
+    }
+
+    public FlowTaskExecutorIdAndCount(String executorId,String executorName,String flowDefinitionId,String flowName,String taskKey,String taskName,long count){
+        this.executorId = executorId;
+        this.executorName = executorName;
+        this.flowDefinitionId = flowDefinitionId;
+        this.flowName = flowName;
+        this.taskKey = taskKey;
+        this.taskName = taskName;
+        this.count = count;
+    }
+
+    public FlowTaskExecutorIdAndCount(String executorId,String flowDefinitionId,String flowName,String taskKey,String taskName,long count){
+        this.executorId = executorId;
+        this.flowDefinitionId = flowDefinitionId;
+        this.flowName = flowName;
+        this.taskKey = taskKey;
+        this.taskName = taskName;
         this.count = count;
     }
 
@@ -39,5 +63,45 @@ public class FlowTaskExecutorIdAndCount implements Serializable {
 
     public void setCount(long count) {
         this.count = count;
+    }
+
+    public String getFlowDefinitionId() {
+        return flowDefinitionId;
+    }
+
+    public void setFlowDefinitionId(String flowDefinitionId) {
+        this.flowDefinitionId = flowDefinitionId;
+    }
+
+    public String getFlowName() {
+        return flowName;
+    }
+
+    public void setFlowName(String flowName) {
+        this.flowName = flowName;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskKey() {
+        return taskKey;
+    }
+
+    public void setTaskKey(String taskKey) {
+        this.taskKey = taskKey;
+    }
+
+    public String getExecutorName() {
+        return executorName;
+    }
+
+    public void setExecutorName(String executorName) {
+        this.executorName = executorName;
     }
 }
