251f027d30234b982a233c0044492d214a162343
何灿坤 <cankun.he@changhong.com>
2019-01-10 14:53:35 +0800

    添加保存固化流程执行人的方法实现

--- flow-service/src/main/java/com/ecmp/flow/service/FlowSolidifyExecutorService.java
+++ flow-service/src/main/java/com/ecmp/flow/service/FlowSolidifyExecutorService.java
@@ -6,10 +6,13 @@ import com.ecmp.flow.api.IFlowSolidifyExecutorService;
 import com.ecmp.flow.dao.FlowSolidifyExecutorDao;
 import com.ecmp.flow.entity.FlowSolidifyExecutor;
 import com.ecmp.flow.vo.FlowSolidifyExecutorVO;
+import com.ecmp.log.util.LogUtil;
 import com.ecmp.vo.ResponseData;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 
@@ -31,17 +34,39 @@ public class FlowSolidifyExecutorService  extends BaseEntityService<FlowSolidify
      * @param businessId
      * @return
      */
-    public ResponseData saveByExecutorVoList(List<FlowSolidifyExecutorVO> executorVoList,
-                             String businessModelCode,String businessId){
+    @Transactional
+    public ResponseData saveByExecutorVoList(List<FlowSolidifyExecutorVO> executorVoList, String businessModelCode,String businessId){
         ResponseData   responseData  = new ResponseData();
+        if(executorVoList==null||executorVoList.size()==0|| StringUtils.isEmpty(businessModelCode)||StringUtils.isEmpty(businessId)){
+            return this.writeErrorLogAndReturnData(null,"请求参数不能为空！");
+        }
+       try{
+           executorVoList.forEach(executorVo->{
+               FlowSolidifyExecutor bean =new FlowSolidifyExecutor();
+               bean.setBusinessCode(businessModelCode);
+               bean.setBusinessId(businessId);
+               bean.setActTaskDefKey(executorVo.getActTaskDefKey());
+               bean.setInstancyStatus(executorVo.getInstancyStatus());
+               bean.setExecutorIds(executorVo.getExecutorIds());
+               flowSolidifyExecutorDao.save(bean);
+           });
+       }catch (Exception e){
+           return this.writeErrorLogAndReturnData(e,"节点信息、紧急状态、执行人不能为空！");
+       }
+        return  responseData;
+    }
 
 
 
 
-
-
-
-      return  responseData;
+    public ResponseData writeErrorLogAndReturnData(Exception e,String msg){
+        if (e!=null) {
+            LogUtil.error(e.getMessage());
+        }
+        ResponseData responseData = new ResponseData();
+        responseData.setSuccess(false);
+        responseData.setMessage(msg);
+        return  responseData;
     }
 
 
