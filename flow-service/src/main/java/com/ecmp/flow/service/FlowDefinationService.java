915601263624d9450dd0ac5dc22101ce2c7e1094
何灿坤 <cankun.he@changhong.com>
2019-01-23 09:24:50 +0800

    优化自动发布

--- flow-service/src/main/java/com/ecmp/flow/service/FlowDefinationService.java
+++ flow-service/src/main/java/com/ecmp/flow/service/FlowDefinationService.java
@@ -526,7 +526,7 @@ public class FlowDefinationService extends BaseEntityService<FlowDefination> imp
             if (userMap != null && !userMap.isEmpty()) {//判断是否选择了下一步的用户
                 Map<String, Object> v = flowStartVO.getVariables();
                 v.putAll(userMap);
-                if (v.get("additionRemark") != null && StringUtils.isNotEmpty(v.get("additionRemark").toString()) && !"null".equalsIgnoreCase(v.get("additionRemark").toString())) {
+                if (v.get("additionRemark") != null && StringUtils.isNotEmpty(v.get("additionRemark").toString()) && !"null".equalsIgnoreCase(v.get("additionRemark").toString().trim())) {
                     v.put("workCaption", v.get("workCaption").toString() + "【附加说明：" + v.get("additionRemark").toString() + "】");
                     flowStartVO.getVariables().put("workCaption", v.get("workCaption").toString());
                 }
