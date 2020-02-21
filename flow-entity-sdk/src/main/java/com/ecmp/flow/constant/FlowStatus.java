a1e017d7169818c80f1ba55e92e95eb16bdaa7e7
何灿坤 <cankun.he@changhong.com>
2020-02-21 11:18:36 +0800

    sdk新增

--- flow-entity-sdk/src/main/java/com/ecmp/flow/constant/FlowStatus.java
+++ flow-entity-sdk/src/main/java/com/ecmp/flow/constant/FlowStatus.java
@@ -21,17 +21,14 @@ public enum FlowStatus implements Serializable{
     /**
      * 未进入流程
      */
-    @Remark("初始化状态")
     INIT("init"),
     /**
      * 流程处理中
      */
-    @Remark("流程中")
     INPROCESS("inProcess"),
     /**
      * 流程处理完成
      */
-    @Remark("流程处理完成")
     COMPLETED("completed");
 
     private String value;
